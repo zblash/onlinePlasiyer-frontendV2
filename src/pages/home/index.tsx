9d8fc032053018aecb998ee2ba9a1940f73b94a1
yasintz <yasinntazeoglu@gmail.com>
2019-11-19 17:36:12 +0300

    added fetcher component for category horizontal list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react';
 import { Container } from '~/components/ui';
-import { CategoryHorizontalList } from '~/components/common/category-horizontal-list';
-import { useQuery } from '~/services/query-context/context';
-import { queryEndpoints } from '~/services/query-context/query-endpoints';
+import { CategoryHorizontalListFetcher } from '~/fetcher-components/common/category-horizontal-list';
 
 interface HomeProps {}
 
 const Home: React.SFC<HomeProps> = props => {
-  const { data: allCategories } = useQuery(queryEndpoints.getCategories, {
-    variables: { type: 'all' },
-    defaultValue: [],
-  });
-
-  const categoriesMap = allCategories
-    .filter(category => !category.subCategory)
-    .map(category => ({
-      ...category,
-      subCategories: allCategories.filter(subCategory => subCategory.parentId === category.id),
-    }));
-
   return (
     <Container>
-      <CategoryHorizontalList categories={categoriesMap} shouldUseProductsPageLink />
+      <CategoryHorizontalListFetcher shouldUseProductsPageLink />
     </Container>
   );
 };
