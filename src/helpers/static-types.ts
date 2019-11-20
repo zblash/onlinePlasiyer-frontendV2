241b4c95184a3bac3c7179c4cdf5ff5f319a30db
yasintz <yasinntazeoglu@gmail.com>
2019-11-20 19:35:48 +0300

    fixed static-types

--- src/helpers/static-types.ts
+++ src/helpers/static-types.ts
@@ -1,14 +1,4 @@
-export type TranslationKeys =
-  | 'common.login'
-  | 'common.create'
-  | 'common.update'
-  | 'common.next'
-  | 'common.add'
-  | 'product-popup.category-input-placeholder'
-  | 'not-connect-page.message'
-  | 'popups.delete-category.are-you-sure-question'
-  | 'popups.delete-category.cancel'
-  | 'popups.delete-category.delete';
+export type TranslationKeys='common.login'|'common.create'|'common.update'|'common.next'|'common.add'|'product-popup.category-input-placeholder'|'not-connect-page.message'|'popups.delete-category.are-you-sure-question'|'popups.delete-category.cancel'|'popups.delete-category.delete';
 export interface StaticColorType {
   white: string;
   whiteSolid: string;
@@ -18,7 +8,7 @@ export interface StaticColorType {
   lightGray: string;
   danger: string;
   deletePopup: StaticColorDeletePopup;
-}
+};
 interface StaticColorDeletePopup {
   danger: string;
 }
