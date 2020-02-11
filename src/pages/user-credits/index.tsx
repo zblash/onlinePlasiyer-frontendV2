85393151fd8643f4bab19d62a34b27a80d9cf8a8
Yusuf Can Celik <yusufcancelik@hotmail.com>
2020-02-11 03:30:31 +0300

    added pages

--- src/pages/user-credits/index.tsx
+++ src/pages/user-credits/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import styled, { colors } from '~/styled';
-import { useTranslation } from '~/i18n';
 
 /* UserCredits Helpers */
 interface UserCreditsProps {}
@@ -15,7 +14,6 @@ const StyledUserCreditsWrapper = styled.div`
 /* UserCredits Component  */
 function UserCredits(props: React.PropsWithChildren<UserCreditsProps>) {
   /* UserCredits Variables */
-  const { t } = useTranslation();
   /* UserCredits Callbacks */
 
   /* UserCredits Lifecycle  */
